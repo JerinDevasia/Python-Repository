6efba3e440c1913e03bffc9033705b094ee4f14d
Rahul Krishna <77327652+rxhulkrishna@users.noreply.github.com>
2022-11-18 15:50:40 +0530

    Add files via upload

--- Lab cycle 2/5.py
+++ Lab cycle 2/5.py
@@ -1,7 +1,9 @@
-n = 4
-x = 0
-for i in range(1,n+1):
-    x = i
-    print("\n")
-    for y in range(1,i+1):
-        print(y * x,end=" ")
+def pyramid(n):
+    x = 0
+    for i in range(1,n+1):
+        x = i
+        for y in range(1,i+1):
+            print(y * x,end=" ")
+        print("\n")
+n = int(input("Enter number of steps : "))
+pyramid(n)
