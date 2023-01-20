5e9bfd4a488a1ff87d82376fdfd5ab9b1e5d9f0e
Rahul Krishna <77327652+rxhulkrishna@users.noreply.github.com>
2023-01-20 14:44:00 +0530

    Add files via upload

--- Lab cycle 5/dictfile.py
+++ Lab cycle 5/dictfile.py
@@ -1,4 +1,12 @@
 import csv
-mydict = {'1':"hello",'2':"world",'3':"hi"}
+my_dict = {'1':"hello",'2':"world",'3':"hi"}
 
-with open('new.csv')
+with open('new.csv','w') as f:
+    for key in my_dict.keys():
+        string = str(key) + "," + str(my_dict[key]) + "\n"
+        f.write(string)
+
+with open('new.csv',mode="r") as file:
+    csvfile = csv.reader(file)
+    for line in csvfile:
+        print(line)
